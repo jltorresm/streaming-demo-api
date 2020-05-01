2665aae6be043d3e56f11989abadbac5c16e6f79
Jose Torres <jose.torres.tm@gmail.com>
2020-05-01 09:16:07 -0500

    Store the video's metadata in the DB, upload it to the remote location in S3 and create the transcoder job.

--- src/Action/PostVideo.php
+++ src/Action/PostVideo.php
@@ -2,25 +2,28 @@
 
 namespace MediaServer\Action;
 
+use Aws\S3\S3Client;
+use MediaServer\Service\VideoTranscoder;
+use mef\Sql\Driver\SqlDriver;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class PostVideo implements ActionInterface
+class PostVideo extends AbstractAction
 {
 	public function run(Request $request): Response
 	{
-		/*******************************************************************************
-		 * NOTE: For the sake of this demo I allowed a higher memory consumption for
-		 *       post and upload sizes.
+		/*************************************************************************
+		 * NOTE: For the sake of this demo I allowed a higher memory consumption
+		 *       for post and upload sizes.
 		 *
 		 *             post_max_size --> 100M
 		 *       upload_max_filesize --> 100M
-		 ******************************************************************************/
+		 ************************************************************************/
 
-		/*******************************************************************************
+		/*************************************************************************
 		 * Get the uploaded VIDEO and validate a few things.
-		 ******************************************************************************/
+		 ************************************************************************/
 		/**
 		 * @var $video UploadedFile
 		 */
@@ -45,17 +48,59 @@ class PostVideo implements ActionInterface
 			die("Bad Request :: missing name");
 		}
 
-		/*******************************************************************************
+		/*************************************************************************
+		 * Define all the video metadata we need.
+		 ************************************************************************/
+		$videoMeta = [
+			"name"       => $video->getClientOriginalName(),
+			"uuid"       => hash("sha256", $video->openFile("r")->fread($video->getSize())),
+			"type"       => $video->getMimeType(),
+			"created_dt" => gmdate(DATE_RFC3339),
+		];
+
+		/*************************************************************************
 		 * Move the accepted media to a new, more permanent location.
-		 ******************************************************************************/
+		 ************************************************************************/
+		$uploadDirectory = $this->config["uploads"]["local"];
+		if ($this->config["uploads"]["type"] == "remote")
+		{
+			// Register the stream wrapper from an S3Client object
+			$client = new S3Client($this->config["aws"]["clientOptions"]);
+			$client->registerStreamWrapper();
+
+			// Register the remote upload path
+			$uploadDirectory = $this->config["uploads"]["remote"];
+		}
+
 		$generatedName = sprintf(
-			"%s.%s.%s",
-			gmdate("YmdHis"),
-			$video->getClientOriginalName(),
+			"%s.%s",
+			$videoMeta["uuid"],
 			explode("/", $video->getMimeType())[1]
 		);
 
-		$video->move(__DIR__ . "/../../uploads", $generatedName);
+		$video->move($uploadDirectory, $generatedName);
+
+		/*************************************************************************
+		 * Now we save the references to DB.
+		 ************************************************************************/
+		/**
+		 * @var $database SqlDriver
+		 */
+		$database = $this->config["database"]();
+		$database->insert()->into("video")->namedValues($videoMeta)->execute();
+
+		/*************************************************************************
+		 * And handle transcoding for remote uploads.
+		 *
+		 * TODO: This is a big candidate to be moved to an asynchronous handler
+		 *       either via jobs, lambda functions, AWS pipelines/triggers, or any
+		 *       other async solution.
+		 ************************************************************************/
+		if ($this->config["uploads"]["type"] == "remote")
+		{
+			$transcoder = new VideoTranscoder($this->config);
+			$transcoder->transcode($videoMeta);
+		}
 
 		return new Response("OK");
 	}
---
+++ src/Service/VideoTranscoder.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace MediaServer\Service;
+
+use Aws\MediaConvert\MediaConvertClient;
+use Closure;
+
+class VideoTranscoder
+{
+	/**
+	 * @var MediaConvertClient Aws MediaConverter client handle.
+	 */
+	private MediaConvertClient $mediaConvertClient;
+
+	/**
+	 * @var string Where the original files are stored.
+	 */
+	private string $source;
+
+	/**
+	 * @var string Where the transcoded files are stored.
+	 */
+	private string $destination;
+
+	/**
+	 * @var Closure Closure that generates the MediaConvert job spec.
+	 */
+	private Closure $mediaConvertJobGenerator;
+
+	/**
+	 * VideoTranscoder constructor.
+	 *
+	 * @param array $config App configuration.
+	 */
+	public function __construct(array $config)
+	{
+		$this->source = $config["aws"]["mediaConvert"]["source"];
+		$this->destination = $config["aws"]["mediaConvert"]["destination"];
+		$this->mediaConvertJobGenerator = $config["aws"]["mediaConvert"]["job"];
+		$this->mediaConvertClient = $this->getMediaConvertClient($config["aws"]["clientOptions"]);
+	}
+
+	private function getMediaConvertClient(array $baseOptions): MediaConvertClient
+	{
+		$tmpClient = new MediaConvertClient($baseOptions);
+		$customEndpoints = $tmpClient->describeEndpoints();
+		$baseOptions["endpoint"] = $customEndpoints['Endpoints'][0]['Url'];
+		unset($tmpClient);
+		return new MediaConvertClient($baseOptions);
+	}
+
+	/**
+	 * @param array $video Some info about the video to identify it and be able to start the transcoding process.
+	 */
+	public function transcode(array $video)
+	{
+		$source = sprintf("%s%s.%s", $this->source, $video["uuid"], explode("/", $video["type"])[1]);
+		$jobSpec = ($this->mediaConvertJobGenerator)($source, $this->destination);
+
+		// Transcode original video to SD & HD HLS format
+		$this->mediaConvertClient->createJob($jobSpec);
+	}
+}
