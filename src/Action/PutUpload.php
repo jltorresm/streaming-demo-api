311ab57e2a336691f8ab56e24b1635ae74ca800f
Jose Torres <jose.torres.tm@gmail.com>
2020-05-06 08:35:42 -0500

    Update PutUpload to not call the transcoding process manually.

--- src/Action/PutUpload.php
+++ src/Action/PutUpload.php
@@ -2,13 +2,24 @@
 
 namespace MediaServer\Action;
 
-use MediaServer\Service\VideoTranscoder;
 use mef\Sql\Driver\SqlDriver;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class PutUpload. Saves upload metadata to the DB.
+ *
+ * @package MediaServer\Action
+ */
 class PutUpload extends AbstractAction
 {
+	/**
+	 * Execute the request handler.
+	 *
+	 * @param Request $request The request to handle.
+	 *
+	 * @return Response
+	 */
 	public function run(Request $request): Response
 	{
 		/*************************************************************************
@@ -22,7 +33,6 @@ class PutUpload extends AbstractAction
 		$rawData = json_decode($request->getContent(), true);
 		$data = filter_var_array($rawData, $sanitizer);
 
-
 		/*************************************************************************
 		 * Define all the video metadata we need and save the reference in the DB.
 		 ************************************************************************/
@@ -35,17 +45,19 @@ class PutUpload extends AbstractAction
 		$database->insert()->into("video")->namedValues($videoMeta)->execute();
 
 		/*************************************************************************
-		 * And handle transcoding for remote uploads.
+		 * Note: We are just handling metadata at this point. The transcoding
+		 *       process is handled asynchronously in the AWS infrastructure
+		 *       via lambda functions and S3 triggers.
 		 *
-		 * TODO: This is a big candidate to be moved to an asynchronous handler
-		 *       either via jobs, lambda functions, AWS pipelines/triggers, or any
-		 *       other async solution.
+		 * If, for any reason, we want to do the transcode call manually,
+		 * uncomment the lines below. Just make sure to de-activate the triggers
+		 * in AWS first, otherwise you could be doing the process twice.
 		 ************************************************************************/
-		if ($this->config["uploads"]["type"] == "remote")
-		{
-			$transcoder = new VideoTranscoder($this->config);
-			$transcoder->transcode($videoMeta);
-		}
+		// if ($this->config["uploads"]["type"] == "remote")
+		// {
+		// 	$transcoder = new VideoTranscoder($this->config);
+		// 	$transcoder->transcode($videoMeta);
+		// }
 
 		return new Response("OK");
 	}
