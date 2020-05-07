9cc81e0f09a1a242061cac06f905306a33084222
Jose Torres <jose.torres.tm@gmail.com>
2020-05-07 08:05:39 -0500

    Add an error flag to the DB.

--- etc/schema.sql
+++ etc/schema.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS `video` (
-	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(100) NULL,
-	`uuid` char(64) NOT NULL,
-	`type` varchar(50) NOT NULL,
-    `processed` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'set to true after transcoding occurs',
-	`created_dt` DATETIME NOT NULL,
+	`id`         INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`name`       VARCHAR(100)     NULL,
+	`uuid`       CHAR(64)         NOT NULL,
+	`type`       VARCHAR(50)      NOT NULL,
+	`processed`  TINYINT(1)       NOT NULL DEFAULT '0' COMMENT 'set to true after transcoding occurs',
+	`error`      TINYINT(1)       NOT NULL DEFAULT '0' COMMENT 'set to true if transcoding fails',
+	`created_dt` DATETIME         NOT NULL,
 	PRIMARY KEY (`id`),
-	UNIQUE KEY `video_sha256_idx` (`uuid`)
-) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
+	UNIQUE KEY `video_uuid_idx` (`uuid`)
+) ENGINE = InnoDB DEFAULT CHARSET = `utf8mb4`;
